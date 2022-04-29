d0b7ea9435a8b94fd8de2ec97c59544de498efad
MarcosAugusto47 <marcosbsb2014@gmail.com>
2022-04-29 19:43:53 -0300

    new files organization considering bollinger code

--- utils/functions_bollinger.py
+++ utils/functions_bollinger.py
@@ -26,6 +26,26 @@ def fetch_data(exchange_client, ticker, candle_duration_in_min):
 
     return ticker_df
 
+def get_trade_recommendation_MACD(ticker_df):
+    #rsi_oversold = RSI_OVERSOLD
+    #rsi_overbought = RSI_OVERBOUGHT
+    macd_result = 'WAIT'
+    #bollinger_result = 'WAIT'
+    #final_result = 'WAIT'
+
+    # BUY or SELL based on MACD crossover points and the RSI value at that point
+    macd, signal, hist = talib.MACD(ticker_df['close'], fastperiod = 12, slowperiod = 26, signalperiod = 9)
+    last_hist = hist.iloc[-1]
+    prev_hist = hist.iloc[-2]
+
+    if not np.isnan(prev_hist) and not np.isnan(last_hist):
+        # If hist value has changed from negative to positive or vice versa, it indicates a crossover
+        macd_crossover = (abs(last_hist + prev_hist)) != (abs(last_hist) + abs(prev_hist))
+        if macd_crossover:
+            macd_result = 'BUY' if last_hist > 0 else 'SELL'
+
+    return macd_result
+
 # STEP 2: COMPUTE THE TECHNICAL INDICATORS & APPLY THE TRADING STRATEGY
 def get_trade_recommendation(ticker_df, rsi_oversold, rsi_overbought):
 
@@ -53,6 +73,36 @@ def get_trade_recommendation(ticker_df, rsi_oversold, rsi_overbought):
             final_result = 'SELL'
     print("MACD Result:", macd_result, "Final Result:", final_result)
     return final_result if final_result == macd_result else 'WAIT'
+
+def confirm_sell_operation(ticker_df, weight):
+    bollinger_result = 'WAIT'
+    
+    # 20-window moving average 
+    ticker_df['sma_bb'] = ticker_df['close'].rolling(window=20).mean()
+    # upper and lower bollinger bands: SMA +/- 2 * standard deviation
+    ticker_df['stddev'] = ticker_df['close'].rolling(window=20).std()
+    ticker_df['upper_boll1'] = ticker_df['sma_bb'] + (1 * ticker_df['stddev'])
+    ticker_df['weighted_upper_boll1'] = ticker_df['sma_bb'] + (1 * weight* ticker_df['stddev'])
+    ticker_df['upper_boll2'] = ticker_df['sma_bb'] + (2 * ticker_df['stddev'])
+    ticker_df['lower_boll1'] = ticker_df['sma_bb'] - (1 * ticker_df['stddev'])
+    ticker_df['lower_boll2'] = ticker_df['sma_bb'] - (2 * ticker_df['stddev'])
+    upper_boll1_price = ticker_df['upper_boll1'].iloc[-1]
+    weighted_upper_boll1_price = ticker_df['weighted_upper_boll1'].iloc[-1]
+    upper_boll2_price = ticker_df['upper_boll2'].iloc[-1]
+    lower_boll1_price = ticker_df['lower_boll1'].iloc[-1]
+    lower_boll2_price = ticker_df['lower_boll2'].iloc[-1]
+
+    last_price = ticker_df['close'].iloc[-1]
+
+    if not np.isnan(last_price):
+        sigma_reached = last_price >= weighted_upper_boll1_price
+        if sigma_reached:
+            bollinger_result = 'SELL'
+    
+    print(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}: "
+      f"Last price: {last_price}; Bollinger 1 Sigma: {upper_boll1_price}; Weighted Bollinger 1 Sigma:{weighted_upper_boll1_price}")
+    
+    return bollinger_result
 
 # STEP 3: EXECUTE THE TRADE
 def execute_trade(binance_client, trade_rec_type, trading_ticker, investiment_amount_dollars, decimals_quantity, holding_quantity):
